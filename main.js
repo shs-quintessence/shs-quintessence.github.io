84cb117808ec27e479447c0d2d063fc08904bdf8
Adrian Gomes <adriangomes@Hugos-iMac.local>
2015-10-10 17:55:51 -0400

    changed everything

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
+window.onscroll = function() {navScroll(), headerScroll()};
 
+function navScroll() {
+    var offsetHeight = document.getElementById('container').offsetHeight;
+    var navFadeHeight = offsetHeight / 4;
+    var nav = document.getElementById('nav');
+
+    if (document.body.scrollTop || document.documentElement.scrollTop > navFadeHeight) {
+        nav.style.background = "rgba(0, 0, 0, 1)";
+    } else {
+        nav.style.background = "rgba(0, 0, 0, 0)";
+    }
+}
+
+function headerScroll() {
+
+}
