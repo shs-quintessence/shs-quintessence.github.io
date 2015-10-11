c294c563c2798442f81342f6ea2b4d6a6a22c220
Adrian Gomes <adriangomes@Hugos-iMac.local>
2015-10-11 17:29:22 -0400

    added scrolling anchor navigation

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-window.onscroll = function() {navScroll(), headerScroll()};
+window.onscroll = function() {navScroll()};
 
 function navScroll() {
     var offsetHeight = document.getElementById('container').offsetHeight;
@@ -6,12 +6,36 @@ function navScroll() {
     var nav = document.getElementById('nav');
 
     if (document.body.scrollTop || document.documentElement.scrollTop > navFadeHeight) {
-        nav.style.background = "rgba(0, 0, 0, 1)";
+        nav.style.background = 'rgba(0, 0, 0, 1)';
     } else {
-        nav.style.background = "rgba(0, 0, 0, 0)";
+        nav.style.background = 'rgba(0, 0, 0, 0)';
     }
 }
 
-function headerScroll() {
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top - 81
+      }, 900);
+        return false;
+      }
+    }
+  });
+});
 
+var mobileNav;
+
+function mobileNavOpen() {
+    mobileNav = document.getElementById('navSide');
+    mobileNav.style.right = "0";
+    mobileNav.style.opacity = "1";
+}
+
+function mobileNavClose() {
+    mobileNav.style.right = "-30vw";
+    mobileNav.style.opacity = "0";
 }
