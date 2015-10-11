2f908fe9414ececdfa1ac9c35f3b8e8b592e4554
ahgomes <adrianhgomes@gmail.com>
2015-10-11 13:48:20 -0400

    added fake loading

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
+setTimeout(function(){
+    $( "body" ).addClass( "loaded" )
+    $(this).scrollTop(0);
+}, 5000);
+
+setTimeout(function(){
+    $( "body" ).addClass( "filled" )
+}, 4900);
+
 window.onscroll = function() {navScroll()};
 
 function navScroll() {
     var offsetHeight = document.getElementById('container').offsetHeight;
     var navFadeHeight = offsetHeight / 4;
     var nav = document.getElementById('nav');
-    var title = document.getElementById('headerTitle');
 
     if (document.body.scrollTop || document.documentElement.scrollTop > navFadeHeight) {
         nav.style.background = 'rgba(0, 0, 0, 1)';
     } else {
         nav.style.background = 'rgba(0, 0, 0, 0)';
+
     }
 }
 
@@ -29,16 +38,23 @@ $(function() {
 });
 
 var mobileNav;
+var clickOut;
 
 function mobileNavOpen() {
     mobileNav = document.getElementById('navSide');
+    clickOut = document.getElementById('cover');
+
     mobileNav.style.right = "0";
     mobileNav.style.opacity = "1";
+    clickOut.style.opacity = '0.5';
+    clickOut.style.top = '0';
 }
 
 function mobileNavClose() {
-    mobileNav.style.right = "-30vw";
+    mobileNav.style.right = "-100%";
     mobileNav.style.opacity = "0";
+    clickOut.style.opacity = '0';
+    clickOut.style.top = '-100%';
 }
 
 var screenWidth = screen.width;
